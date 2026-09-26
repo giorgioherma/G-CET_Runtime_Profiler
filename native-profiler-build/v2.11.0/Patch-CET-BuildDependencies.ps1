4eea6c3d34ac7f9e78f6ad5aa1b69384cc86353c
giorgioherma <giorgio.herma@hotmail.com>
2026-09-26 19:01:59 +0200

    source: publish recovered native profiler v2.11.0 build material

---
+++ native-profiler-build/v2.11.0/Patch-CET-BuildDependencies.ps1
@@ -0,0 +1,5 @@
+throw @"
+v2.7 does not use the v2.5/v2.6 compatibility dependency patch.
+It reconstructs the xmake-repo state from the CET v1.37.1 release date instead.
+Run Build-Profiler.ps1.
+"@
